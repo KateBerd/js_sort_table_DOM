ff02bdbec15d76da1bd5ea64c3ca114ff71a68f3
Kateryna Berdnikova <berdekaterinasp@gmail.com>
2025-01-17 15:07:54 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const headers = [...thead.queryselectorAll('th')];
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const tbody = document.querySelector('tbody');
+
+    const arrayOfChildren = [...tbody.querySelectorAll('tr')];
+    let sortedChildren = [];
+
+    if (index === 0 || index === 1) {
+      sortedChildren = arrayOfChildren.sort((person1, person2) => {
+        const per1 = person1.children[index].textContent;
+        const per2 = person2.children[index].textContent;
+
+        per1.localCompare(per2);
+      });
+    }
+
+    if (index === 2 || index === 3) {
+      sortedChildren = arrayOfChildren.sort(
+        (person1, person2) =>
+          parseFloat(person1.children[index].textContent) -
+          parseFloat(person2.children[index].textContent),
+      );
+    }
+
+    tbody.innerHTML = '';
+    sortedChildren.forEach((child) => tbody.append(child));
+  });
+});
