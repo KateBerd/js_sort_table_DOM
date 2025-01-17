97bca506707c4c42592524a5cba23bdfe21fc079
Kateryna Berdnikova <berdekaterinasp@gmail.com>
2025-01-17 16:27:34 +0200

    solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const thead = document.querySelector('thead');
-const headers = [...thead.queryselectorAll('th')];
+const headers = [...thead.querySelectorAll('th')];
 
 headers.forEach((header, index) => {
   header.addEventListener('click', () => {
@@ -15,15 +15,15 @@ headers.forEach((header, index) => {
         const per1 = person1.children[index].textContent;
         const per2 = person2.children[index].textContent;
 
-        per1.localCompare(per2);
+        return per1.localeCompare(per2);
       });
     }
 
     if (index === 2 || index === 3) {
       sortedChildren = arrayOfChildren.sort(
         (person1, person2) =>
-          parseFloat(person1.children[index].textContent) -
-          parseFloat(person2.children[index].textContent),
+          parseFloat(person1.children[index].textContent.replace('$', '')) -
+          parseFloat(person2.children[index].textContent.replace('$', '')),
       );
     }
 
